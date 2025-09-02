d04cc779650c981e7f2d91819f807e7a19c1684e
muhammed rifan <rifanmuhammed173@gmail.com>
2025-09-03 00:53:23 +0530

    updation in subcription

--- Router/user.route.js
+++ Router/user.route.js
@@ -3,8 +3,6 @@ const userRouter= express()
 const GymController= require('../Controller/gym.controller')
 const UserController= require('../Controller/user.controller')
 
-
-
 // Client Authentication
 userRouter.post('/login',UserController.ClientLoginWeb)
 
@@ -17,17 +15,10 @@ userRouter.post('/updateuser',UserController.EditUser)
 userRouter.get('/get-userlist',UserController.GetUserList)
 userRouter.get('/get-players-listbygymid',UserController.getGymPlayersListByGymid)
 
-// membership plans
+// Note: Membership plans functionality has been moved to gym-owner routes
+// Use /gym-owner/subscriptions for subscription management
 
-userRouter.post('/insert-membership-plans',MembershipPlansController.createGymPlayerPlan)
-userRouter.post('/update-membership-plans',MembershipPlansController.updateGymPlayerPlan)
-userRouter.get('/get-membership-plans',MembershipPlansController.getGymPlayerPlansByGymid)
-
-
-
-// token 
+// Token Management
 userRouter.get('/refresh-token',UserController.refreshToken)
-
-
 
 module.exports = userRouter
